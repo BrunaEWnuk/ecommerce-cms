5f8da8d9d5e918830eb4c0d1b5618ad29355fd9e
BrunaEWnuk <brunawnukm@gmail.com>
2025-10-27 23:18:23 -0300

    Implementação do layout e components do products e brands pt. 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import { Route, Routes } from "react-router-dom"
-import { CategoryLayout } from "./cases/categories/components/category-layout"
-import { CategoryForm } from "./cases/categories/components/category-form"
+import { Route, Routes } from "react-router-dom";
+import { CategoryLayout } from "./cases/categories/components/category-layout";
+import { CategoryForm } from "./cases/categories/components/category-form";
+import { BrandLayout } from "./cases/brands/components/brand-layout";
+import { BrandForm } from "./cases/brands/components/brand-form";
+import { ProductLayout } from "./cases/products/components/product-layout";
+import { ProductForm } from "./cases/products/components/product-form";
 
 function App() {
-
   return (
     <div className="wrapper">
-
-    <main>
-      <Routes>
-        <Route path="/categories" element={<CategoryLayout />} >
-        <Route path="new" element={<CategoryForm />} />
-        <Route path=":id" element={<CategoryForm />} />
-      </Route>
-      </Routes>
-    </main>
-
+      <main>
+        <Routes>
+          <Route path="/categories" element={<CategoryLayout />}>
+            <Route path="new" element={<CategoryForm />} />
+            <Route path=":id" element={<CategoryForm />} />
+          </Route>
+          <Route path="/brands" element={<BrandLayout />}>
+            <Route path="new" element={<BrandForm />} />
+            <Route path=":id" element={<BrandForm />} />
+          </Route>
+          <Route path="/products" element={<ProductLayout />}>
+            <Route path="new" element={<ProductForm />} />
+            <Route path=":id" element={<ProductForm />} />
+          </Route>
+        </Routes>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
